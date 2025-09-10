86c04d7a7669ab03d0b28546a4762dd513f31eb6
Nils Olav Handegard <nilsolav@imr.no>
2025-09-10 10:30:26 +0200

    updated data organisation with Scottish data

--- setup_folders.py
+++ setup_folders.py
@@ -4,16 +4,30 @@ import pandas as pd
 # List of survey
 df = pd.read_csv('cruises.csv', sep=';')
 
-# Create folders
+root = '/data/s3/MACWIN-scratch/'
+
+# Create folders for raw data (only scottish data since imr data are on cruise data s3)
 for index, row in df.iterrows():
-    print(row)
-    print(index)
+    if row['shipname'] in ['Scotia', 'Sunbeam']:
+        cruise = row['cruise']
+        year = row['year']
+        # List of survey names
+        raw = os.path.join(root, str(year), cruise, 'ACOUSTIC', 'EK60', 'EK60_RAWDATA')
+        print(raw)
+        os.makedirs(raw, exist_ok=True)
+    
+# This is the folder for the Korona files from the workshop    
+for index, row in df.iterrows():
     cruise = row['cruise']
     year = row['year']
-    # List of survey names
-    raw = os.path.join(str(year), cruise, 'ACOUSTIC', 'EK80', 'EK80_RAWDATA')
-    os.makedirs(raw, exist_ok=True)
-    
-    korona = os.path.join(str(year), cruise, 'ACOUSTIC', 'LSSS', 'KORONA')
+    korona = os.path.join(root, str(year), cruise, 'ACOUSTIC', 'LSSS', 'WORK')
+    print(korona)
     os.makedirs(korona, exist_ok=True)
 
+# This is the folders for the output from the Korona procesing
+for index, row in df.iterrows():
+    cruise = row['cruise']
+    year = row['year']
+    korona = os.path.join(root, str(year), cruise, 'ACOUSTIC', 'LSSS', 'KORONA')
+    print(korona)
+    os.makedirs(korona, exist_ok=True)
