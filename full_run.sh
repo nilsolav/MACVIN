e984f3aa64b901b100284decc3fe297c5329587e
Nils Olav Handegard <nilsolav@imr.no>
2026-03-15 10:45:43 +0100

    Updated exclude list.

--- full_run.sh
+++ full_run.sh
@@ -1,44 +1,44 @@
 #
 
 # S2000012_PGOSARS_1024 sv_ok, atc_ok, running report
-export CRUISE=S2000012_PGOSARS_1024
+#export CRUISE=S2000012_PGOSARS_1024
 #uv run macvin-ek500conversion --cruise "$CRUISE"
 #uv run macvin-idxprocessing --cruise "$CRUISE"
 #uv run macvin-preprocessing --cruise "$CRUISE"
 #uv run macvin-atcprocessing --cruise "$CRUISE"
 #uv run macvin-reports --cruise "$CRUISE"
-uv run macvin-lufreports --cruise "$CRUISE"
-uv run macvin-status --cruise "$CRUISE"
+#uv run macvin-lufreports --cruise "$CRUISE"
+#uv run macvin-status --cruise "$CRUISE"
 
 # S2001013_PGOSARS_1024 updated_filelist
-export CRUISE=S2001013_PGOSARS_1024
-uv run macvin-ek500conversion --cruise "$CRUISE"
-uv run macvin-idxprocessing --cruise "$CRUISE"
-uv run macvin-preprocessing --cruise "$CRUISE"
-uv run macvin-atcprocessing --cruise "$CRUISE"
-uv run macvin-reports --cruise "$CRUISE"
-uv run macvin-lufreports --cruise "$CRUISE"
-uv run macvin-status --cruise "$CRUISE"
+#export CRUISE=S2001013_PGOSARS_1024
+#uv run macvin-ek500conversion --cruise "$CRUISE"
+#uv run macvin-idxprocessing --cruise "$CRUISE"
+#uv run macvin-preprocessing --cruise "$CRUISE"
+#uv run macvin-atcprocessing --cruise "$CRUISE"
+#uv run macvin-reports --cruise "$CRUISE"
+#uv run macvin-lufreports --cruise "$CRUISE"
+#uv run macvin-status --cruise "$CRUISE"
 
 # S2002015_PGOSARS_1024 sv_ok, atc_ok
-export CRUISE=S2002015_PGOSARS_1024
+#export CRUISE=S2002015_PGOSARS_1024
 #uv run macvin-ek500conversion --cruise "$CRUISE"
 #uv run macvin-idxprocessing --cruise "$CRUISE"
 #uv run macvin-preprocessing --cruise "$CRUISE"
 #uv run macvin-atcprocessing --cruise "$CRUISE"
-uv run macvin-reports --cruise "$CRUISE"
-uv run macvin-lufreports --cruise "$CRUISE"
-uv run macvin-status --cruise "$CRUISE"
+#uv run macvin-reports --cruise "$CRUISE"
+#uv run macvin-lufreports --cruise "$CRUISE"
+#uv run macvin-status --cruise "$CRUISE"
 
 # S2003112_PGOSARS_4174 sv_ok, atc_ok
-export CRUISE=S2003112_PGOSARS_4174
+#export CRUISE=S2003112_PGOSARS_4174
 #uv run macvin-ek500conversion --cruise "$CRUISE"
 #uv run macvin-idxprocessing --cruise "$CRUISE"
 #uv run macvin-preprocessing --cruise "$CRUISE"
 #uv run macvin-atcprocessing --cruise "$CRUISE"
-uv run macvin-reports --cruise "$CRUISE"
-uv run macvin-lufreports --cruise "$CRUISE"
-uv run macvin-status --cruise "$CRUISE"
+#uv run macvin-reports --cruise "$CRUISE"
+#uv run macvin-lufreports --cruise "$CRUISE"
+#uv run macvin-status --cruise "$CRUISE"
 
 # S2004113_PGOSARS_4174 not complete
 export CRUISE=S2004113_PGOSARS_4174
@@ -52,6 +52,7 @@ uv run macvin-status --cruise "$CRUISE"
 
 # S2006212_PJOHANHJORT_1019 not complete
 export CRUISE=S2006212_PJOHANHJORT_1019
+uv run macvin-ek500conversion --cruise "$CRUISE"
 uv run macvin-idxprocessing --cruise "$CRUISE"
 uv run macvin-preprocessing --cruise "$CRUISE"
 uv run macvin-atcprocessing --cruise "$CRUISE"
