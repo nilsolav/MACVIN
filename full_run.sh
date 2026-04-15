5bb9f744033f350e8628deba8cd815a9ba5bb25a
Nils Olav Handegard <nilsolav@imr.no>
2026-04-15 15:16:34 +0200

    Updated run script.

--- full_run.sh
+++ full_run.sh
@@ -1,7 +1,7 @@
 #
 
 # S2000012_PGOSARS_1024 sv_ok, atc_ok, running report
-#export CRUISE=S2000012_PGOSARS_1024
+export CRUISE=S2000012_PGOSARS_1024
 #uv run macvin-ek500conversion --cruise "$CRUISE"
 #uv run macvin-idxprocessing --cruise "$CRUISE"
 #uv run macvin-preprocessing --cruise "$CRUISE"
@@ -11,7 +11,7 @@ uv run macvin-lufreports --cruise "$CRUISE"
 uv run macvin-status --cruise "$CRUISE"
 
 # S2001013_PGOSARS_1024 updated_filelist
-#export CRUISE=S2001013_PGOSARS_1024
+export CRUISE=S2001013_PGOSARS_1024
 #uv run macvin-ek500conversion --cruise "$CRUISE"
 #uv run macvin-idxprocessing --cruise "$CRUISE"
 #uv run macvin-preprocessing --cruise "$CRUISE"
@@ -21,7 +21,7 @@ uv run macvin-lufreports --cruise "$CRUISE"
 uv run macvin-status --cruise "$CRUISE"
 
 # S2002015_PGOSARS_1024 sv_ok, atc_ok
-#export CRUISE=S2002015_PGOSARS_1024
+export CRUISE=S2002015_PGOSARS_1024
 #uv run macvin-ek500conversion --cruise "$CRUISE"
 #uv run macvin-idxprocessing --cruise "$CRUISE"
 #uv run macvin-preprocessing --cruise "$CRUISE"
@@ -31,7 +31,7 @@ uv run macvin-lufreports --cruise "$CRUISE"
 uv run macvin-status --cruise "$CRUISE"
 
 # S2003112_PGOSARS_4174 sv_ok, atc_ok
-#export CRUISE=S2003112_PGOSARS_4174
+export CRUISE=S2003112_PGOSARS_4174
 #uv run macvin-ek500conversion --cruise "$CRUISE"
 #uv run macvin-idxprocessing --cruise "$CRUISE"
 #uv run macvin-preprocessing --cruise "$CRUISE"
@@ -41,7 +41,7 @@ uv run macvin-lufreports --cruise "$CRUISE"
 uv run macvin-status --cruise "$CRUISE"
 
 # S2004113_PGOSARS_4174 not complete
-#export CRUISE=S2004113_PGOSARS_4174
+export CRUISE=S2004113_PGOSARS_4174
 #uv run macvin-ek500conversion --cruise "$CRUISE"
 #uv run macvin-idxprocessing --cruise "$CRUISE"
 #uv run macvin-preprocessing --cruise "$CRUISE"
@@ -51,7 +51,7 @@ uv run macvin-lufreports --cruise "$CRUISE"
 uv run macvin-status --cruise "$CRUISE"
 
 # S2006212_PJOHANHJORT_1019 not complete
-#export CRUISE=S2006212_PJOHANHJORT_1019
+export CRUISE=S2006212_PJOHANHJORT_1019
 #uv run macvin-ek500conversion --cruise "$CRUISE"
 #uv run macvin-idxprocessing --cruise "$CRUISE"
 #uv run macvin-preprocessing --cruise "$CRUISE"
