a21723243d070073859023bf7f414cb237ad774a
Nils Olav Handegard <nilsolav@hi.no>
2026-05-16 15:00:15 +0200

    Version used for Hector.

--- full_run.sh
+++ full_run.sh
@@ -60,16 +60,6 @@
 #uv run macvin-lufreports --cruise "$CRUISE"
 #uv run macvin-status --cruise "$CRUISE"
 
-# S2005114_PGOSARS_4174  'ping_time' not strictly increasing in /PREPROCESSING/tokt2005114-D20051109-T021146.nc
-export CRUISE=S2005114_PGOSARS_4174
-uv run macvin-idxprocessing --cruise "$CRUISE"
-uv run macvin-preprocessing --cruise "$CRUISE"
-uv run macvin-atcprocessing --cruise "$CRUISE"
-##uv run macvin-preprocess2zarr --cruise "$CRUISE"
-##uv run macvin-atc2zarr --cruise "$CRUISE"
-uv run macvin-reports --cruise "$CRUISE"
-uv run macvin-lufreports --cruise "$CRUISE"
-uv run macvin-status --cruise "$CRUISE"
 
 # S2006212_PJOHANHJORT_1019
 #export CRUISE=S2006212_PJOHANHJORT_1019
@@ -82,39 +72,28 @@ uv run macvin-status --cruise "$CRUISE"
 #uv run macvin-lufreports --cruise "$CRUISE"
 #uv run macvin-status --cruise "$CRUISE"
 
-# 'ping_time' not strictly increasing in /PREPROCESSING/tokt2005114-D20051109-T021146.nc
-export CRUISE=S2007211_PJOHANHJORT_1019
-uv run macvin-idxprocessing --cruise "$CRUISE"
-uv run macvin-preprocessing --cruise "$CRUISE"
-uv run macvin-atcprocessing --cruise "$CRUISE"
+# 'ping_time' not strictly increasing in /PREPROCESSING/2012843-D20121002-T202452.nc
+#export CRUISE=S2012842_PCHRISTINAE_2704
+#uv run macvin-idxprocessing --cruise "$CRUISE"
+#uv run macvin-preprocessing --cruise "$CRUISE"
+#uv run macvin-atcprocessing --cruise "$CRUISE"
 ##uv run macvin-preprocess2zarr --cruise "$CRUISE"
 ##uv run macvin-atc2zarr --cruise "$CRUISE"
-uv run macvin-reports --cruise "$CRUISE"
-uv run macvin-lufreports --cruise "$CRUISE"
-uv run macvin-status --cruise "$CRUISE"
-
-# 'ping_time' not strictly increasing in /PREPROCESSING/2012843-D20121002-T202452.nc
-export CRUISE=S2012842_PCHRISTINAE_2704
-uv run macvin-idxprocessing --cruise "$CRUISE"
-uv run macvin-preprocessing --cruise "$CRUISE"
-uv run macvin-atcprocessing --cruise "$CRUISE"
-#uv run macvin-preprocess2zarr --cruise "$CRUISE"
-#uv run macvin-atc2zarr --cruise "$CRUISE"
-uv run macvin-reports --cruise "$CRUISE"
-uv run macvin-lufreports --cruise "$CRUISE"
-uv run macvin-status --cruise "$CRUISE"
+#uv run macvin-reports --cruise "$CRUISE"
+#uv run macvin-lufreports --cruise "$CRUISE"
+#uv run macvin-status --cruise "$CRUISE"
 
 
 # S2012843_PBRENNHOLM_4405
-export CRUISE=S2012843_PBRENNHOLM_4405 # ValueError: conflicting sizes for dimension 'ping_time': length 1333071 on 'ping_time' and length 1332990 on {'category': 'annotation', 'ping_time': 'annotation', 'range': 'annotation'}
-uv run macvin-idxprocessing --cruise "$CRUISE"
-uv run macvin-preprocessing --cruise "$CRUISE"
-uv run macvin-atcprocessing --cruise "$CRUISE"
-#uv run macvin-preprocess2zarr --cruise "$CRUISE"
-#uv run macvin-atc2zarr --cruise "$CRUISE"
-uv run macvin-reports --cruise "$CRUISE"
-uv run macvin-lufreports --cruise "$CRUISE"
-uv run macvin-status --cruise "$CRUISE"
+#export CRUISE=S2012843_PBRENNHOLM_4405 # ValueError: conflicting sizes for dimension 'ping_time': length 1333071 on 'ping_time' and length 1332990 on {'category': 'annotation', 'ping_time': 'annotation', 'range': 'annotation'}
+#uv run macvin-idxprocessing --cruise "$CRUISE"
+#uv run macvin-preprocessing --cruise "$CRUISE"
+#uv run macvin-atcprocessing --cruise "$CRUISE"
+##uv run macvin-preprocess2zarr --cruise "$CRUISE"
+##uv run macvin-atc2zarr --cruise "$CRUISE"
+#uv run macvin-reports --cruise "$CRUISE"
+#uv run macvin-lufreports --cruise "$CRUISE"
+#uv run macvin-status --cruise "$CRUISE"
 
 # S1504S_PSCOTIA_MXHR6
 #export CRUISE=S1504S_PSCOTIA_MXHR6
@@ -137,3 +116,25 @@ uv run macvin-status --cruise "$CRUISE"
 #uv run macvin-reports --cruise "$CRUISE"
 #uv run macvin-lufreports --cruise "$CRUISE"
 #uv run macvin-status --cruise "$CRUISE"
+
+# S2005114_PGOSARS_4174  'ping_time' not strictly increasing in /PREPROCESSING/tokt2005114-D20051109-T021146.nc
+#export CRUISE=S2005114_PGOSARS_4174
+#uv run macvin-idxprocessing --cruise "$CRUISE"
+#uv run macvin-preprocessing --cruise "$CRUISE"
+#uv run macvin-atcprocessing --cruise "$CRUISE"
+##uv run macvin-preprocess2zarr --cruise "$CRUISE"
+##uv run macvin-atc2zarr --cruise "$CRUISE"
+#uv run macvin-reports --cruise "$CRUISE"
+#uv run macvin-lufreports --cruise "$CRUISE"
+#uv run macvin-status --cruise "$CRUISE"
+
+# 'ping_time' not strictly increasing in /PREPROCESSING/tokt2005114-D20051109-T021146.nc
+export CRUISE=S2007211_PJOHANHJORT_1019
+uv run macvin-idxprocessing --cruise "$CRUISE"
+uv run macvin-preprocessing --cruise "$CRUISE"
+uv run macvin-atcprocessing --cruise "$CRUISE"
+##uv run macvin-preprocess2zarr --cruise "$CRUISE"
+##uv run macvin-atc2zarr --cruise "$CRUISE"
+uv run macvin-reports --cruise "$CRUISE"
+uv run macvin-lufreports --cruise "$CRUISE"
+uv run macvin-status --cruise "$CRUISE"
